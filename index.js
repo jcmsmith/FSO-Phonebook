91c2b36780eb24a8df6e8799dc081d56ed9a473a
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-04-23 01:30:51 -0700

    Deploy to Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { request } = require('express');
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 
 const app = express()
@@ -39,6 +40,7 @@ const generateId = () => {
 }
 
 /*  Middleware */
+app.use(cors())
 app.use(express.json())
 
 app.use(morgan('tiny', {
@@ -133,7 +135,9 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
 
