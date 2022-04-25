e4f1dc0506be45fbc8aec9be85ca3c904a719df0
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-04-24 21:00:05 -0700

    Exercise 3.20
    Add custom Mongoose validator for numbers

--- models/entry.js
+++ models/entry.js
@@ -13,6 +13,16 @@ mongoose
       console.log('error connecting to MongoDB', error.message)
   })
 
+const numberValidator = (num) => {
+    const re1 = /^\d*$/
+    const re2 = /^\d{2,3}-\d*$/
+    const re3 = /^\d{3}-\d{3}-\d{4}$/
+
+    const result = re1.test(num) || re2.test(num) || re3.test(num)
+    console.log('numberValidator test results (test1, test2, test3, result):', re1.test(num), re2.test(num), re3.test(num), result)
+    return result
+}
+
 const entrySchema = new mongoose.Schema({
     name: {
       type: String,
@@ -21,8 +31,9 @@ const entrySchema = new mongoose.Schema({
     },
     number: {
       type: String,
-      minLength: 6,
-      required: true
+      minLength: 8,
+      required: true,
+      validate: [numberValidator, "Please use one of the following formats: 12345678, 12-345678, 123-45678, or 123-456-7890"]
     }
 })
 
